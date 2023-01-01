9c8a2a15e3cf08d705f7434d6f12a5652cb8ea67
Alexander Plavin <alexander@plav.in>
2023-01-01 00:00:00 +0000

    first integrations

--- src/AccessorsExtra.jl
+++ src/AccessorsExtra.jl
@@ -1,3 +1,37 @@
 module AccessorsExtra
 
+using Reexport
+@reexport using Accessors
+using ConstructionBase
+using Requires
+
+
+function __init__()
+    @require StructArrays = "09ab397b-f2b6-538f-b94a-2f83cf4a842a" begin
+        using .StructArrays
+
+        Accessors.set(x::StructArray, ::typeof(StructArrays.components), v) where {T} = StructArray(v)
+
+        # if only Accessors.set is needed, not full setproperties:
+        # Accessors.set(x::StructArray, o::Accessors.PropertyLens, v) = set(x, o ∘ StructArrays.components, v)
+        ConstructionBase.getproperties(x::StructArray) = StructArrays.components(x)
+        function ConstructionBase.setproperties(x::StructArray, patch::NamedTuple)
+            nt = getproperties(x)
+            nt_new = merge(nt, patch)
+            ConstructionBase.validate_setproperties_result(nt_new, nt, x, patch)
+            StructArray(nt_new)
+        end
+
+        Accessors.insert(x::StructArray, o::Accessors.PropertyLens, v) = insert(x, o ∘ StructArrays.components, v)
+        Accessors.delete(x::StructArray, o::Accessors.PropertyLens) = delete(x, o ∘ StructArrays.components)
+    end
+
+    @require SplitApplyCombine = "03a91e81-4c3e-53e1-a0a4-9c0c8f19dd66" begin
+        import .SplitApplyCombine: MappedArray
+
+        # https://github.com/JuliaObjects/Accessors.jl/pull/53
+        Base.setindex!(ma::MappedArray, val, ix) = parent(ma)[ix] = set(parent(ma)[ix], ma.f, val)
+    end
+end
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,27 @@
 using AccessorsExtra
 using Test
+using StructArrays
+using SplitApplyCombine
 
-@testset begin
+
+@testset "structarrays" begin
+    s = StructArray(a=[1, 2, 3])
+    @test @insert(StructArrays.components(s).b = 10:12) == [(a=1, b=10), (a=2, b=11), (a=3, b=12)]
+    @test setproperties(s, a=10:12) == StructArray(a=10:12)
+    @test_throws ArgumentError setproperties(s, b=10:12)
+    @test @set(s.a = 10:12) == StructArray(a=10:12)
+    @test @insert(s.b = 10:12) == [(a=1, b=10), (a=2, b=11), (a=3, b=12)]
+    @test @delete(@insert(s.b = 10:12).a) == StructArray(b=10:12)
+    @test @delete(s.a) == NamedTuple{(), Tuple{}}[]
+end
+
+@testset "mapview" begin
+    X = [(a=1, b=2), (a=3, b=4)]
+    Y = mapview(@optic(_.b), X)
+    @test Y == [2, 4]
+    Y[2] = 100
+    @test Y == [2, 100]
+    @test X == [(a=1, b=2), (a=3, b=100)]
 end
 
 
