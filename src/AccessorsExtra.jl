4e5008ddd32af1e858effd05e0033c09829fbcb9
Alexander Plavin <alexander@plav.in>
2023-01-01 00:00:00 +0000

    simplify

--- src/AccessorsExtra.jl
+++ src/AccessorsExtra.jl
@@ -15,19 +15,14 @@ function __init__()
         # if only Accessors.set is needed, not full setproperties:
         # Accessors.set(x::StructArray, o::Accessors.PropertyLens, v) = set(x, o ∘ StructArrays.components, v)
         ConstructionBase.getproperties(x::StructArray) = StructArrays.components(x)
-        function ConstructionBase.setproperties(x::StructArray, patch::NamedTuple)
-            nt = getproperties(x)
-            nt_new = merge(nt, patch)
-            ConstructionBase.validate_setproperties_result(nt_new, nt, x, patch)
-            StructArray(nt_new)
-        end
+        ConstructionBase.setproperties(x::StructArray, patch::NamedTuple) = @modify(cs -> setproperties(cs, patch), StructArrays.components(x))
 
         Accessors.insert(x::StructArray, o::Accessors.PropertyLens, v) = insert(x, o ∘ StructArrays.components, v)
         Accessors.delete(x::StructArray, o::Accessors.PropertyLens) = delete(x, o ∘ StructArrays.components)
     end
 
     @require SplitApplyCombine = "03a91e81-4c3e-53e1-a0a4-9c0c8f19dd66" begin
-        import .SplitApplyCombine: MappedArray
+        using .SplitApplyCombine: MappedArray
 
         # https://github.com/JuliaObjects/Accessors.jl/pull/53
         Base.setindex!(ma::MappedArray, val, ix) = parent(ma)[ix] = set(parent(ma)[ix], ma.f, val)
