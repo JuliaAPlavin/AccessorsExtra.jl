6daa5a0f97ba463572a558bef59aebff59c25d6d
Alexander Plavin <alexander@plav.in>
2024-01-01 00:00:00 +0000

    named-concatoptic

--- src/concatoptic.jl
+++ src/concatoptic.jl
@@ -3,6 +3,7 @@ struct ConcatOptics{OS}
 end
 
 concat(optics...) = ConcatOptics(optics)
+concat(; optics...) = ConcatOptics(values(optics))
 const ++ = concat
 
 Accessors.OpticStyle(::Type{<:ConcatOptics}) = Accessors.ModifyBased()
@@ -11,8 +12,11 @@ Accessors.OpticStyle(::Type{<:ConcatOptics}) = Accessors.ModifyBased()
 function Accessors.getall(obj, co::ConcatOptics)
     map(co.optics) do o
         getall(obj, o)
-    end |> Accessors._reduce_concat
+    end |> _reduce_concat
 end
+
+_reduce_concat(xs) = Accessors._reduce_concat(xs)
+_reduce_concat(xs::NamedTuple) = map(only, xs)
 
 function Accessors.modify(f, obj, co::ConcatOptics)
     foldl(co.optics; init=obj) do obj, o
@@ -20,7 +24,7 @@ function Accessors.modify(f, obj, co::ConcatOptics)
     end
 end
 
-function Accessors.setall(obj, co::ConcatOptics, vals)
+function Accessors.setall(obj, co::ConcatOptics{<:Tuple}, vals)
     lengths = map(co.optics) do o
         Accessors._staticlength(getall(obj, o))
     end
@@ -30,11 +34,23 @@ function Accessors.setall(obj, co::ConcatOptics, vals)
     end
 end
 
+function Accessors.setall(obj, co::ConcatOptics{<:NamedTuple}, vals::NamedTuple{KS}) where {KS}
+    foreach(NamedTuple{KS}(co.optics), vals) do o, vss
+        @assert length(getall(obj, o)) == 1
+    end
+    foldl(map(tuple, NamedTuple{KS}(co.optics), vals); init=obj) do obj, (o, vss)
+        set(obj, o, vss)
+    end
+end
 
-function Base.show(io::IO, co::ConcatOptics)
+
+function Base.show(io::IO, co::ConcatOptics{<:Tuple})
+    print(io, "(")
     for (i, o) in enumerate(co.optics)
         i == 1 || print(io, " ++ ")
         show(io, o)
     end
+    print(io, ")")
 end
-Base.show(io::IO, ::MIME"text/plain", optic::ConcatOptics) = show(io, optic)
+Base.show(io::IO, ::MIME"text/plain", optic::ConcatOptics{<:Tuple}) = show(io, optic)
+
