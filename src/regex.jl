4db593b9b51645e66882c782c2e3b4cbeb3c5828
Alexander Plavin <alexander@plav.in>
2024-01-01 00:00:00 +0000

    remove old impl

--- src/regex.jl
+++ src/regex.jl
@@ -64,50 +64,6 @@ function setall(obj::AbstractString, o::Base.Fix1{typeof(eachmatch)}, vs::Tuple{
     end
 end
 
-# alternative implementation that overload compositions - below
-# any advantages?
-
-# struct EachmatchElements
-#     r::Regex
-# end
-# OpticStyle(::Type{<:EachmatchElements}) = ModifyBased()
-
-# Base.:∘(i::Elements, o::Base.Fix1{typeof(eachmatch)}) = EachmatchElements(o.x)
-# Base.:∘(c::ComposedFunction{<:Any, <:Elements}, o::Base.Fix1{typeof(eachmatch)}) = c.outer ∘ EachmatchElements(o.x)
-
-# getall(s::AbstractString, o::EachmatchElements) = eachmatch(o.r, s)
-
-# function modify(f, s::AbstractString, o::EachmatchElements)
-#     replacements = map(eachmatch(o.r, s)) do m
-#         sub = m.match
-#         rng = (sub.offset+1):(sub.offset+sub.ncodeunits)
-#         x = f(m)
-#         repl = if x isa AbstractString
-#             x
-#         else
-#             @assert x === m
-#             m.match
-#         end
-#         rng => repl::AbstractString
-#     end
-#     @assert issorted(replacements, by=r -> first(first(r)))
-#     foldl(reverse(replacements); init=s) do s, (rng, v)
-#         @set s[FlexIx(rng)] = v
-#     end
-# end
-
-# function setall(s::AbstractString, o::EachmatchElements, v)
-#     replacements = map(eachmatch(o.r, s), v) do m, x
-#         sub = m.match
-#         rng = (sub.offset+1):(sub.offset+sub.ncodeunits)
-#         rng => x::AbstractString
-#     end
-#     @assert issorted(replacements, by=r -> first(first(r)))
-#     foldl(reverse(replacements); init=s) do s, (rng, v)
-#         @set s[FlexIx(rng)] = v
-#     end
-# end
-
 set(m::RegexMatch, o::IndexLens, v) = _set_regexmatch(m, o, v)
 set(m::RegexMatch, o::PropertyLens, v) = _set_regexmatch(m, o, v)
 function _set_regexmatch(m::RegexMatch, o::Union{IndexLens,PropertyLens}, v::AbstractString)
