68780da7a9d3134a996a93a0af01a53f354c0d81
Alexander Plavin <alexander@plav.in>
2024-01-01 00:00:00 +0000

    remove duplicated method

--- src/modifymany.jl
+++ src/modifymany.jl
@@ -4,7 +4,6 @@
 @inline modify(f, A::Vector, ::Elements, Bs...) = map(f, A, Bs...)
 
 @inline modify(f, A::NamedTuple{KS}, ::Properties, B::NamedTuple) where {KS} = map(f, A, B[KS])
-@inline modify(f, A::NamedTuple, ::Properties, B::NamedTuple) = error("modify not supported for different keys: $(keys(A)) vs $(keys(B))")
 @inline modify(f, A::NTuple{N,Any}, ::Properties, B::NTuple{N,Any}) where {N} = map(f, A, B)
 @inline modify(f, A::Tuple, ::Properties, B::Tuple) = error("modify not supported for different lengths: $(length(A)) vs $(length(B))")
 @inline modify(f, A, ::Properties, Bs...) = setproperties(A, modify(f, getproperties(A), Properties(), getproperties.(Bs)...))
