137aa86eaccf64e0921bba07751f8789bb5e8c58
Alexander Plavin <alexander@plav.in>
2024-01-01 00:00:00 +0000

    cleaner keys/values

--- src/AccessorsExtra.jl
+++ src/AccessorsExtra.jl
@@ -151,16 +151,25 @@ InverseFunctions.inverse(f::Base.Fix1{typeof(getindex)}) = Base.Fix2(findfirst, 
 InverseFunctions.inverse(f::ComposedFunction{<:Base.Fix2{typeof(findfirst)}, typeof(isequal)}) = Base.Fix1(getindex, f.outer.x)
 
 
-# optics inspired by https://juliaobjects.github.io/Accessors.jl/stable/examples/custom_optics/
-struct Keys end
-OpticStyle(::Type{Keys}) = ModifyBased()
-modify(f, obj::Dict, ::Keys) = Dict(f(k) => v for (k, v) in pairs(obj))
-modify(f, obj::NamedTuple{NS}, ::Keys) where {NS} = NamedTuple{map(f, NS)}(values(obj))
+# inspired by https://juliaobjects.github.io/Accessors.jl/stable/examples/custom_optics/ but cleaner? :)
 
-struct Values end
-OpticStyle(::Type{Values}) = ModifyBased()
-modify(f, obj::Union{AbstractArray, Tuple, NamedTuple}, ::Values) = map(f, obj)
-function modify(f, dict::Dict, ::Values)
+# special wrapper type, can only be modify'ed with Elements()
+struct KVPWrapper{TO,T}
+    o::TO
+    obj::T
+end
+
+modify(f, obj, o::Union{typeof(keys),typeof(values),typeof(pairs)}) = f(KVPWrapper(o, obj))
+
+### keys
+modify(f, obj::KVPWrapper{typeof(keys), <:Dict}, ::Elements) = Dict(f(k) => v for (k, v) in pairs(obj.obj))
+modify(f, obj::KVPWrapper{typeof(keys), <:NamedTuple{NS}}, ::Elements) where {NS} = NamedTuple{map(f, NS)}(values(obj.obj))
+
+### values
+modify(f, obj::KVPWrapper{typeof(values), <:Union{AbstractArray, Tuple, NamedTuple}}, ::Elements) = map(f, obj.obj)
+
+function modify(f, obj::KVPWrapper{typeof(values), <:Dict}, ::Elements)
+    dict = obj.obj
     V = Core.Compiler.return_type(f, Tuple{valtype(dict)})
     vals = dict.vals
     newvals = similar(vals, V)
@@ -172,36 +181,29 @@ function modify(f, dict::Dict, ::Values)
     setproperties(dict, vals=newvals)
 end
 
-struct Pairs end
-OpticStyle(::Type{Pairs}) = ModifyBased()
-modify(f, obj::AbstractArray, ::Pairs) = map(eachindex(obj), obj) do i, x
-    p = f(i => x)
-    @assert first(p) == i
-    last(p)
-end
-modify(f, obj::Tuple, ::Pairs) = ntuple(length(obj)) do i
-    p = f(i => obj[i])
-    @assert first(p) == i
-    last(p)
-end
-modify(f, obj::NamedTuple, ::Pairs) = map(keys(obj), values(obj)) do k, v
-    p = f(k => v)
-    @assert first(p) == k
-    last(p)
-end |> NamedTuple{keys(obj)}
-modify(f, obj::Dict, ::Pairs) = Dict(f(p) for p in pairs(obj))
+### pairs
+modify(f, obj::KVPWrapper{typeof(pairs), <:AbstractArray}, ::Elements) =
+    map(eachindex(obj.obj), obj.obj) do i, x
+        p = f(i => x)
+        @assert first(p) == i
+        last(p)
+    end
+modify(f, obj::KVPWrapper{typeof(pairs), <:Tuple}, ::Elements) =
+    ntuple(length(obj.obj)) do i
+        p = f(i => obj.obj[i])
+        @assert first(p) == i
+        last(p)
+    end
+modify(f, obj::KVPWrapper{typeof(pairs), <:NamedTuple{NS}}, ::Elements) where {NS} =
+    map(NS, values(obj.obj)) do k, v
+        p = f(k => v)
+        @assert first(p) == k
+        last(p)
+    end |> NamedTuple{NS}
+modify(f, obj::KVPWrapper{typeof(pairs), <:Dict}, ::Elements) = Dict(f(p)::Pair for p in pairs(obj.obj))
 
 
-for (f, o) in [
-    (values, Values),
-    (keys, Keys),
-    (pairs, Pairs),
-]
-    @eval Base.:∘(i::Elements, o::typeof($f)) = $o()
-    @eval Base.:∘(c::ComposedFunction{<:Any, <:Elements}, o::typeof($f)) = c.outer ∘ $o()
-end
-
-
+# see https://github.com/JuliaObjects/ConstructionBase.jl/pull/47
 function ConstructionBase.setproperties(d::Dict, patch::NamedTuple{(:vals,)})
     K = keytype(d)
     V = eltype(patch.vals)
