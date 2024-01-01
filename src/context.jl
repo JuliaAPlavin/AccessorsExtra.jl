9038b45cb971335f923ecf02d2fc6b6798f6f726
Alexander Plavin <alexander@plav.in>
2024-01-01 00:00:00 +0000

    context minor

--- src/context.jl
+++ src/context.jl
@@ -24,22 +24,23 @@ OpticStyle(::Type{<:SelfContext}) = ModifyBased()
 Base.show(io::IO, co::SelfContext) = print(io, "selfcontext(", co.f, ")")
 selfcontext(f=identity) = SelfContext(f)
  
-struct WithContext{I,V}
+struct ValWithContext{I,V}
     i::I
     v::V
 end
-Base.first(ix::WithContext) = ix.i
-Base.last(ix::WithContext) = ix.v
-Base.iterate(ix::WithContext, args...) = iterate(ix.i => ix.v, args...)
+Base.first(ix::ValWithContext) = ix.i
+Base.last(ix::ValWithContext) = ix.v
+Base.iterate(ix::ValWithContext, args...) = iterate(ix.i => ix.v, args...)
+Base.show(io::IO, ix::ValWithContext) = print(io, ix.i, " ⇒ ", ix.v)
 
 
 struct _ContextValOnly{I} <: ContextOptic
     i::I
 end
 OpticStyle(::Type{<:_ContextValOnly}) = ModifyBased()
-modify(f, obj, o::_ContextValOnly) = _unpack_val(f(WithContext(o.i, obj)), o.i)
+modify(f, obj, o::_ContextValOnly) = _unpack_val(f(ValWithContext(o.i, obj)), o.i)
 _unpack_val(x, i) = x
-_unpack_val(x::WithContext, i) = (@assert x.i == i; x.v)
+_unpack_val(x::ValWithContext, i) = (@assert x.i == i; x.v)
 
 modify(f, obj, o::SelfContext) = modify(f, obj, _ContextValOnly(o.f(obj)))
 
@@ -67,9 +68,9 @@ end
 
 function modify(f, obj::NamedTuple{KS}, ::Keyed{Elements}) where {KS}
     res = map(keys(obj), values(obj)) do i, v
-        f(WithContext(i, v))
+        f(ValWithContext(i, v))
     end
-    all(x -> x isa WithContext, res) ?
+    all(x -> x isa ValWithContext, res) ?
         NamedTuple{first.(res)}(last.(res)) :
         NamedTuple{KS}(res)
 end
@@ -100,12 +101,12 @@ end
 function modify(f, obj, o::KeepContext)
     modify(f, obj, o.o)
 end
-function modify(f, obj::WithContext, o::KeepContext)
+function modify(f, obj::ValWithContext, o::KeepContext)
     if OpticStyle(o.o) isa SetBased
         x = o.o(obj.v)
-        fx = f(WithContext(obj.i, x))
-        fx isa WithContext ?
-            WithContext(fx.i, set(obj.v, o.o, fx.v)) :
+        fx = f(ValWithContext(obj.i, x))
+        fx isa ValWithContext ?
+            ValWithContext(fx.i, set(obj.v, o.o, fx.v)) :
             set(obj.v, o.o, fx)
     else
         modify(f, obj.v, _ContextValOnly(obj.i) ∘ o.o)
